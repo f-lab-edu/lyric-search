e05109804b3cbf56d738389f1b1c843e24b6cc7c
sunghwancode <sunghwancode@gmail.com>
2024-02-13 04:45:57 +0900

    feat: 초기세팅

---
+++ src/main/java/com/example/lyricsearch/LyricSearchApplication.java
@@ -0,0 +1,13 @@
+package com.example.lyricsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LyricSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LyricSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lyricsearch/LyricSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lyricsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LyricSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
